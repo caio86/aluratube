34babd98c6cd970e6c3bfc91505e3017c66e79b5
Caio86 <80562020+caio86@users.noreply.github.com>
2022-11-10 16:53:54 -0300

    Aula 4

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+## Como está o nosso projeto?
+- `_app.js`: Carrega o setup base do projeto
+  - Aqui fica os Providers de informação do nosso projeto
+  - CSSReset
--- pages/video.js
+++ pages/video.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { ColorModeContext } from "../src/components/Menu/components/ColorMode"
+import Head from "next/head"
 
 export default function Video() {
   const contexto = React.useContext(ColorModeContext)
 
   return (
     <div>
+      <Head>
+        <title>Videos</title>
+      </Head>
       Video!
       {contexto.mode}
       <button onClick={() => contexto.toggleMode()}>
